d7c5c573ff9663bc239f8e30b273ab4b43b3a0bb
jpaniag2 <jpaniag2@gmail.com>
2022-03-10 21:04:01 -0500

    Updated SideBarNavStyle Code.

--- src/Components/SideBarNav/SideBarNavStyle.js
+++ src/Components/SideBarNav/SideBarNavStyle.js
@@ -1,19 +1,12 @@
 import styled from 'styled-components'
-import { Link as LinkR } from 'react-router-dom'
 import { Link as LinkScroll } from 'react-scroll'
-import logo from '../../images/profile_image.png'
-// import logo from '../../images/LogoDemo.png'
-// import logo from '../../images/AztecaLogo14.png'
 
 
 export const Navigation = styled.nav`
-    ${'' /* background-color: #222021; */}
     background-color: #181818;
     width: 100%;
     max-width: 250px;
-    ${'' /* height: 60px; */}
     transition: ${({scrollNav}) => (scrollNav ? '0.6s all ease' : '0.8s all ease')};
-    ${'' /* transition: 0.8s all ease; */}
     display: grid;
     justify-content: center;
     align-items: center;
@@ -44,124 +37,7 @@ export const NavBarContainer = styled.div`
     
     padding-bottom: 30px;
 `
-export const BrandContainer = styled.div`
-    display: flex;
-    justify-content: center;
-    background-repeat: no-repeat;
-    background-image: ${`url(${logo})`};
-    background-size: cover;
-    align-items: center;
-    height: 200px;
-    width: 100%;
-    margin-top: 15px;
-    border-radius: 50%;
-`
 
-export const Logo = styled.div`
-    background-image: ${`url(${logo})`};
-    background-size: cover;
-    margin-top: 0px;
-    background-repeat: no-repeat;
-    background-color: transparent;
-    height: 200px;
-    width: 100%;
-    border-radius: 50%;
-    transition: 0.8s all ease;
-
-
-@media screen and (max-width: 1024px){
-    padding: 0px 12px;
-    background-color: transparent;
-    border: none;
-}
-
-@media screen and (max-width: 450px){
-    margin-top: -10px;
-    margin-right: 40px;
-    padding: 0px 12px;
-    background-position: bottom ;
-    background-color: transparent;
-    border: none;
-    height: 80px;
-    width: 90px;
-}
-
-`
-
-export const BusinessName = styled(LinkScroll)`
-    font-size: 5rem;
-    margin-top: 10px;
-    margin-left: 0.5rem;
-    margin-bottom: 12px;
-    color: #fff;
-    justify-self: flex-start;
-
-    align-items: center;
-    align-left: 24px;
-    text-decoration: none;
-    cursor: pointer;
-
-    font-family: 'Bellota Text', cursive;
-    font-weight: 700;
-    line-height: 0px;
-
-
-    @media screen and (max-width: 1024px){
-        border: none;
-        font-size: 2rem;
-        margin-top: 10px;
-        margin-left: 2rem;
-        margin-bottom: 22px;
-        padding: 0px;
-    }
-
-    @media screen and (max-width: 450px){
-        font-size: 2rem;
-    }
-
-`
-
-export const BusinessName2 = styled.div`
-    margin-top: 50px;
-    margin-left: 10px;
-    color: #fff;
-    justify-self: flex-start;
-    font-size: 2rem;
-    display: flex;
-    align-items: center;
-    align-left: 18px;
-    font-weight: bold;
-    text-decoration: none;
-    left: 0;
-
-    @media screen and (max-width: 1024px){
-        border: none;
-        margin-right: 35px;
-        padding-right: 50px;
-        font-size: 2rem;
-    }
-
-    @media screen and (max-width: 450px){
-        font-size: 1.5rem;
-    }
-
-`
-
-
-export const MobileIcon = styled.div`
-    display: none;
-
-    @media screen and (max-width: 1024px){
-        color: #fff;
-        display: block;
-        position: absolute;
-        top: 0;
-        right: 0;
-        transform: translate(-100%, 60%);
-        font-size: 1.8rem;
-        cursor: pointer;
-    }
-`
 
 
 export const NavigationMap = styled.ul`
@@ -207,36 +83,4 @@ export const NavigationLinks = styled(LinkScroll)`
             transition: 0.5s all ease;
         }
 
-`
-
-
-export const NavigationButton = styled.nav`
-    display: flex;
-    align-items: center;
-    padding: 5px 15px;
-
-    @media screen and (max-width: 1024px){
-        display: none;
-    }
-`
-
-
-export const NavigationButtonLink = styled(LinkR)`
-    border-radius: 50px;
-    background: #fff;
-    white-space: nowrap;
-    padding: 10px 22px;
-    color: #010606;
-    font-size: 16px;
-    outline: none;
-    border: none;
-    cursor: pointer;
-    transition: all 0.2s ease-in-out;
-    text-decoration: none;
-
-    &: hover {
-        transition: all 0.2s ease-in-out;
-        background: #fff;
-        color: #010606;
-    }
 `
